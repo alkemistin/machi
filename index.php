86590b08fdfffb8bd7ae0611d961de1cd4387d8f
Rodrigo <profe.rodrigomadrid@gmail.com>
2016-07-21 09:32:45 +0000

    machi

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Testeando GitHub con Cloud9</title>
+        <link rel="stylesheet" href="css/stylesheet.css" type="text/css" />
+        <link rel="stylesheet" href="css/menu.css" type="text/css" />
+        <script type="text/javascript" src="js/script.js"></script>
+    </head>
+    <body>
+        <header>
+        <?php
+        require_once("menu.html");
+        ?>
+        </header>
+        <div class='raiz'>
+            <h1>Hola Mundo!!!!</h1>
+            <h2>GitHub con Cloud9</h2>
+            <?php
+            echo "<p>Este texto es generado por PHP</p>"
+            
+            ?>
+            <div id="msn"></div>
+        </div>
+    </body>
+</html>
